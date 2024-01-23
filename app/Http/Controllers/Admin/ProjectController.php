af9f7995a7543f2ee2a90023a43036aa06d3e0cc
simaster19 <simaster489321@gmail.com>
2024-01-23 19:32:41 +0700

    Project

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -7,22 +7,46 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index()
-    {
+  public function index() {
+    $project = Project::all()
+    return respose()->view("App.Admin.Project.index", [
+      "datas" => $project
+    ]);
+  }
+  public function create() {
+    return response()->view("App.Admin.Project.create");
+  }
+
+  public function store(Request $request) {
+    $data = Validator::make($request->all, [
+      "cover" => ["required", "mimes:jpg,png,webp"],
+      "jenis_project" => ["required"],
+      "judul" => ["required", "min:5"],
+      "nama_client" => ["required"],
+      "dibuat_dengan" => ["required"]
+      "status" => ["required"]
+    ]);
+
+    if ($data->fails()) {
+      return back()->withErrors($data, "messageError");
     }
-    public function create()
-    {
-    }
-    public function store(Request $request)
-    {
-    }
-    public function edit($id)
-    {
-    }
-    public function update(Request $request, $id)
-    {
-    }
-    public function destroy($id)
-    {
-    }
+
+    $project = Project::create([
+      "cover" => $request->input("cover"),
+      "jenis_project" => $request->input("jenis_project"),
+      "judul" => $request->input("judul"),
+      "slug" => Str::slug($request->input("judul")),
+      "project_url" => $request->input("project_url"),
+      "nama_client" => $request->input("nama_client"),
+      "keterangan" => $request->input("keterangan"),
+      "dibuat_dengan" => $request->input("dibuat_dengan"),
+      "status" => $request->input("status")
+    ]);
+    
+    return back()->with("message", "Data berhasil ditambahkan!");
+  }
+  
+  public function edit($id) {}
+  public function update(Request $request, $id) {}
+  public function destroy($id) {}
 }
